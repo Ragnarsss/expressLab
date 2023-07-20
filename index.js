fca7e5e4fdeb28886d6b0f8327c975727152d80f
CristianRealG <cristian.bulla02@gmail.com>
2023-07-20 05:59:42 -0400

    commiteando

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola");
+});
+
+app.listen(port, () => {
+  console.log(`Example server on port ${port}`);
+});
